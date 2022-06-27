39c28ca032889c300e628e98c69203490c6d484d
Alfin7007 <alfin.7007@gmail.com>
2022-06-27 09:29:52 -0400

    repair upload img

--- config/s3-config.go
+++ config/s3-config.go
@@ -5,12 +5,25 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-func S3Config()(*aws.Config){
-	return &aws.Config{
-		Region : aws.String("us-east-1"),
-		Credentials: credentials.NewStaticCredentials(os.Getenv("S3_KEY"), os.Getenv("S3_SECRET"), ""),
+var theSession *session.Session
+
+//GetConfig Initiatilize config in singleton way
+func GetSession() *session.Session {
+
+	if theSession == nil {
+		theSession = initSession()
 	}
-	
+
+	return theSession
+}
+
+func initSession() *session.Session {
+	newSession := session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String(os.Getenv("AWS_REGION")),
+		Credentials: credentials.NewStaticCredentials(os.Getenv("S3_KEY"), os.Getenv("S3_SECRET"), ""),
+	}))
+	return newSession
 }
