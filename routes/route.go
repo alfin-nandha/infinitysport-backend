ed218abac81cc6149fb825ec09553f2e6f30a5ee
jok3r46 <jsuf046@gmail.com>
2022-06-27 10:46:13 +0700

    solving feature/user, & add CORS

--- routes/route.go
+++ routes/route.go
@@ -5,19 +5,25 @@ import (
 	"project/e-comerce/middlewares"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func New(presenter factory.Presenter) *echo.Echo {
 
 	e := echo.New()
 	e.Pre(middlewares.RemoveTrailingSlash())
-	//e.Pre(middleware.RemoveTrailingSlash())
 
-	e.POST("/auth", presenter.AuthPresenter.Login)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.PATCH, echo.OPTIONS},
+	}))
+
+	e.POST("/signup", presenter.UserPresenter.Insert)
+	e.POST("/login", presenter.AuthPresenter.Login)
 
 	e.GET("/users", presenter.UserPresenter.GetAll, middlewares.JWTMiddleware())
 	e.GET("/users/:id", presenter.UserPresenter.GetDataById, middlewares.JWTMiddleware())
-	e.POST("/users", presenter.UserPresenter.Insert)
 	e.PUT("/users/:id", presenter.UserPresenter.Update)
 	e.DELETE("/users/:id", presenter.UserPresenter.Delete, middlewares.JWTMiddleware())
 
