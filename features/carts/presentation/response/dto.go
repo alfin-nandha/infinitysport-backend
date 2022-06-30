433b0434340ed76483a2e5f30acd467bc4882aba
jok3r46 <jsuf046@gmail.com>
2022-06-30 10:38:15 +0700

    resolving

--- features/carts/presentation/response/dto.go
+++ features/carts/presentation/response/dto.go
@@ -3,20 +3,22 @@ package response
 import "project/e-comerce/features/carts"
 
 type Cart struct {
-	ID        int `json:"id" form:"id"`
-	ProductID int `json:"product_id" form:"product_id"`
-	UserID    int `json:"user_id" form:"user_id"`
-	Price     int `json:"price" form:"price"`
-	Qty       int `json:"quantity" form:"quantity"`
+	ID          int    `json:"id" form:"id"`
+	NameProduct string `json:"product_name" form:"product_name"`
+	Image       string `json:"image" form:"image"`
+	UserID      int    `json:"user_id" form:"user_id"`
+	PriceOrder  int    `json:"price" form:"price"`
+	Qty         int    `json:"quantity" form:"quantity"`
 }
 
 func FromCore(data carts.Core) Cart {
 	return Cart{
-		ID:        data.ID,
-		ProductID: data.ProductID,
-		UserID:    data.UserID,
-		Price:     data.Product.Price,
-		Qty:       data.Qty,
+		ID:          data.ID,
+		UserID:      data.UserID,
+		NameProduct: data.Product.Name,
+		Image:       data.Product.PhotoUrl,
+		PriceOrder:  data.Product.Price,
+		Qty:         data.Qty,
 	}
 }
 
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/aws/aws-sdk-go v1.44.42
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/labstack/echo/v4 v4.7.2
-	github.com/stretchr/testify v1.7.5
+	github.com/stretchr/testify v1.8.0
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
 	gorm.io/driver/mysql v1.3.4
 	gorm.io/gorm v1.23.6
@@ -24,6 +24,7 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
